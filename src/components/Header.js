877918ddbb25b7144562d2cabddd30fed765a700
Chris Ramesh <chris@ticklerfile.com>
2018-03-25 18:00:49 -0700

    added icon fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 // libraries
 import React from 'react';
 import { StyleSheet, View, Text, } from 'react-native';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+// other
+import { colors } from '../themes';
 
 /* Components ==================================================================== */
 const Header = (props) => {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <AnimatedCircularProgress
+        size={100}
+        width={8}
+        fill={80}
+        tintColor={colors.primary1}
+        backgroundColor="#3d5875"
+      >
+        {
+          (fill) => (
+            <Text style={styles.progressText}>
+              {`${Math.round(fill, 0)} days`}
+            </Text>
+          )
+        }
+      </AnimatedCircularProgress>
+
+      <View style={styles.wrapperSettings}>
+        <Icon name="settings" style={styles.iconSettings} />
+      </View>
     </View>    
   );
 };
@@ -15,7 +38,22 @@ const Header = (props) => {
 /* Styles ==================================================================== */
 const styles = StyleSheet.create({
   container: {
-    height: 100,
+    paddingLeft: 8,
+    paddingTop: 8,
+    marginBottom: 32,
+    flexDirection: 'row',
+  },
+  progressText: {
+    color: colors.primary1,
+    fontSize: 15,
+  },
+  iconSettings: {
+    fontSize: 24,
+    color: colors.primary2,
+  },
+  wrapperSettings: {
+    marginRight: 16,
+    marginLeft: 'auto',
   }
 });
 
